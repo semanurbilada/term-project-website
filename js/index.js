4b1bfe4a2e9c56349a29214e1cef34c344e0f89a
semanurbilada <semanurbilada@hotmail.com>
2023-12-05 16:48:18 +0300

    Add gradient background animation & Minor changes

--- js/index.js
+++ js/index.js
@@ -1,19 +1,26 @@
 // Show Sub-Navbar Function
 let menuItems=document.getElementById('menuItems');
+let menuIcon=document.getElementById('menuIcon');
 
 menuItems.style.maxHeight="0px";
 menuItems.style.opacity="0%";
 menuItems.style.pointerEvents="none";
 
-function showMenu(){
+menuIcon.name="menu";
+
+function showMenu() {
     if(menuItems.style.maxHeight=="0px"){
         menuItems.style.maxHeight="400px";
         menuItems.style.opacity="100%";
         menuItems.style.pointerEvents="auto";
+    
+        menuIcon.name="close";
     }
     else {
         menuItems.style.maxHeight="0px"; 
         menuItems.style.opacity="0%";
         menuItems.style.pointerEvents="none";
+
+        menuIcon.name="menu";
     }
 }
