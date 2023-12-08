f8fd15c21ed0d708b2c3e2e1f10aee491261a9ca
semanurbilada <semanurbilada@hotmail.com>
2023-12-08 13:58:26 +0300

    Fix in swiper and navbar bg color change function

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,18 +1,16 @@
 import Swiper from 'swiper';
-import { Navigation, Pagination } from 'swiper/modules';
+import { Pagination } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
 const swiper = new Swiper('.swiper', {
-    modules: [Navigation, Pagination],
+    modules: [ Pagination ],
+    direction: "horizontal",
     loop: true,
     pagination: {
         el: ".swiper-pagination",
-        type: "progressbar",
-    },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+        clickable: true,
+        scrollable: true
     },
 });
