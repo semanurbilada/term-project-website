518efecac7df6d13edb59d0b0e35ef6a13d3d2d7
semanurbilada <semanurbilada@hotmail.com>
2023-12-16 14:03:22 +0300

    Initialize Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,2 +1,49 @@
-// vite.config.js
 export default {}
+
+// Kullanılabilir;
+
+// import { defineConfig } from 'vite';
+// import path from 'path';
+
+// export default defineConfig({
+//     root: path.resolve(__dirname, 'src'),
+//     resolve: {
+//         alias: {
+//             "~swiper": path.resolve(__dirname, "node_modules/swiper"),
+//             '~aos': path.resolve(__dirname, 'node_modules/aos'),
+//         },
+//     },
+//     server: {
+//         port: PORT,
+//         hot: true,
+//     },
+//     base: '/index.html',
+//     outDir: path.resolve(__dirname, 'dist'),
+//     build: {
+//         rollupOptions: {
+//             input: {
+//                 index: path.resolve(__dirname, "src/js/index.js"),
+//                 about: path.resolve(__dirname, "src/html/about.html"),
+//                 blog: path.resolve(__dirname, "src/html/blog.html"),
+//                 cart: path.resolve(__dirname, "src/html/cart.html"),
+//                 contact: path.resolve(__dirname, "src/html/contact.html"),
+//                 favourites: path.resolve(__dirname, "src/html/favourites.html"),
+//                 login: path.resolve(__dirname, "src/html/login.html"),
+//                 shop: path.resolve(__dirname, "src/html/shop.html"),
+//                 signup: path.resolve(__dirname, "src/html/signup.html"),
+//                 support: path.resolve(__dirname, "src/html/support.html"),
+//             },
+//             external: [
+//                 '../src/js/mainSwiper.js',
+//                 '../src/js/gridSwiper.js',
+//                 '../src/js/aos-init.js',
+//                 '../src/css/global.css',
+//                 '../src/css/style.css',
+//             ],
+//         },
+//     },
+//     optimizeDeps: {
+//         entries: [],
+//     },
+//     plugins: [],
+// })
