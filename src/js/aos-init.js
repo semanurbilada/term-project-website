300c48d26da321af117e77a97aee2d1e7ba423d8
semanurbilada <semanurbilada@hotmail.com>
2023-12-16 13:54:57 +0300

    Complete About page with aos animation

---
+++ src/js/aos-init.js
@@ -0,0 +1,28 @@
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+AOS.init({
+    //duration: 1000,
+    disable: false,
+});
+
+// Base aos animation file to use other pages!
+
+// function initializeAOS() {
+//     const screenWidth = window.innerWidth;
+
+//     if (screenWidth > 768) {
+//         AOS.init({
+//         once: true,
+//         duration: 1500,
+//         });
+//     } else {
+//         AOS.init({
+//         disable: true,
+//         });
+//     }
+// }
+// // Call the function on page load
+// initializeAOS();
+
+// window.addEventListener('resize', initializeAOS);
