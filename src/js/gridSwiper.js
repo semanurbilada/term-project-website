531732b65c3815a49b22e4c5c2a42e3640f6aa69
semanurbilada <semanurbilada@hotmail.com>
2023-12-21 12:57:16 +0300

    Add grid swiper to home page

---
+++ src/js/gridSwiper.js
@@ -0,0 +1,41 @@
+import Swiper from 'swiper';
+import { Grid, Autoplay, Pagination } from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/grid';
+import 'swiper/css/pagination';
+
+const swiper = new Swiper('.grid-swiper', {
+    modules: [ Grid, Autoplay, Pagination ],
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+            grid: {
+                rows: 2,
+            },
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 0,
+            grid: {
+                rows: 2,
+            },
+        },
+        480: {
+            slidesPerView: 1,
+            grid: {
+                rows: 2,
+            },
+        }
+    }
+});
--- src/js/mainSwiper.js
+++ src/js/mainSwiper.js
@@ -4,25 +4,14 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 
 const swiper = new Swiper('.main-swiper', {
-    modules: [ Autoplay, Pagination ],
-    loop: true,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-    autoplay: {
-      delay: 4500,
-      disableOnInteraction: false,
-    },
-
-    //TODO: grid-swiper: (popüler yazarlar ksımında kullanılacak - otomatik geçiş)
-    // slidesPerView: 3,
-    // grid: {
-    //     rows: 2,
-    // },
-    // spaceBetween: 30,
-    // pagination: {
-    //     el: ".swiper-pagination",
-    //     clickable: false,
-    // },
+  modules: [ Autoplay, Pagination ],
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  autoplay: {
+    delay: 4500,
+    disableOnInteraction: false,
+  },
 }); 
