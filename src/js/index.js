12d3819c5ce03776b295d297bae1c26effccfc05
semanurbilada <semanurbilada@hotmail.com>
2023-12-22 21:19:43 +0300

    Add sections to home page, needs customization

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 // Show Sub-Navbar Function
-let menuItems=document.getElementById('menuItems');
-let menuIcon=document.getElementById('menuIcon');
+let menuItems = document.getElementById('menuItems');
+let menuIcon = document.getElementById('menuIcon');
 
 // Initial styles
 menuItems.style.maxHeight="0px";
@@ -28,6 +28,7 @@ function showMenu() {
     }
 }
 
+
 // Navbar Color Change Function
 function changeNavbarBg() {
     let navbar = document.getElementById('header');
@@ -41,6 +42,45 @@ function changeNavbarBg() {
 }
 // call the function on scroll
 document.addEventListener('scroll', changeNavbarBg);
+
+
+// Countdown digital clock function
+let countDownDate = new Date("Jan 1, 2024 00:00:00").getTime();
+
+let demo = document.getElementById("demo");
+
+let daysSection = document.getElementById("days");
+let hoursSection = document.getElementById("hours");
+let minutesSection = document.getElementById("minutes");
+let secondsSection = document.getElementById("seconds");
+
+// Update the count down every 1 second
+let updateTime = setInterval(function() {
+
+  // Get today's date and time
+  let now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  let distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the results
+  daysSection.innerHTML = days;
+  hoursSection.innerHTML = hours;
+  minutesSection.innerHTML = minutes;
+  secondsSection.innerHTML = seconds;
+    
+  // If the count down is over, write this text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "The discount is over! Stay Tuned for the next one!";
+  }
+}, 1000);
 
 
 //Cart Add Function - DENEME
