632b8bac845f1a14713faf04e353ef40bf5b1ed2
semanurbilada <semanurbilada@hotmail.com>
2023-12-29 10:46:48 +0300

    Edit shop page, needs content customization

--- src/js/shop.js
+++ src/js/shop.js
@@ -4,21 +4,25 @@ const books = [
     name: 'Dune', 
     img: '/images/item10.jpg',
     writer: 'Frank Herbert', 
+    price: '70$',
     category: 'scienceF' 
   },
   { 
     name: 'Book 2', 
     writer: '', 
+    price: '70$',
     category: 'scienceF'
   },
   { 
     name: 'Electronics 1', 
     writer: '', 
+    price: '70$',
     category: 'comics'
   },
   { 
     name: 'Electronics 2', 
     writer: '', 
+    price: '70$',
     category: 'comics' 
   },
 ];
@@ -27,6 +31,22 @@ const books = [
 function filterBooks(category) {
   const filteredBooks = category === 'all' ? books : books.filter(book => book.category === category);
   displayBooks(filteredBooks);
+  
+  const categoryTitle = document.getElementById('shop-title');
+  categoryTitle.textContent = getCategoryTitle(category);
+}
+//TODO: more customize edilmeli !!!! (içerikle birlikte)
+function getCategoryTitle(category) {
+  switch (category) {
+    case 'all':
+      return 'All Books';
+    case 'scienceF':
+      return 'Science Fiction';
+    case 'comics':
+      return 'Comics';
+    default:
+      return 'All Categories';
+  }
 }
 
 // Function to display books in the books container
@@ -35,12 +55,14 @@ function displayBooks(books) {
   booksContainer.innerHTML = '';
 
   books.forEach(book => {
-    const bookElement = document.createElement('div');
+    const bookElement = document.createElement('div'); 
 
     bookElement.innerHTML = `
       <img src="${book.img}" alt="${book.name}">
-      <p>${book.name}</p>
-      <p>${book.writer}</p>
+      <p class="book-title">${book.name}</p>
+      <p class="book-writer">${book.writer}</p>
+      <p class="book-price">${book.price}</p>
+      <button>Add to Card</button>
     `;
     booksContainer.appendChild(bookElement);
   });
