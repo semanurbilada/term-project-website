fe3b9b02722f574807b44edac2309f9e921874d3
semanurbilada <semanurbilada@hotmail.com>
2024-01-02 21:36:16 +0300

    Complete data in js

--- src/js/data.js
+++ src/js/data.js
@@ -1,10 +1,25 @@
+// Data array of books;
 const books = [
+    { 
+        name: 'My Inventions', 
+        img: '/images/item17.jpg',
+        writer: 'Nikola Tesla', 
+        price: '20.00$',
+        category: 'classic'
+    },
     { 
         name: 'Dune', 
         img: '/images/item10.jpg',
         writer: 'Frank Herbert', 
         price: '70.00$',
         category: 'fiction' 
+    },
+    { 
+        name: 'Pride and Prejudice', 
+        img: '/images/item15.jpg',
+        writer: 'Jane Austen', 
+        price: '40.00$',
+        category: 'classic' 
     },
     { 
         name: 'The Institute',
@@ -28,6 +43,34 @@ const books = [
         category: 'fiction'
     },
     { 
+        name: 'Harry Potter and the Chamber of Secrets', 
+        img: '/images/item26.jpg',
+        writer: 'J.K. Rowling', 
+        price: '90.00$',
+        category: 'fiction'
+    },
+    { 
+        name: 'Icebraker', 
+        img: '/images/item28.jpg',
+        writer: 'Hannah Grace', 
+        price: '15.00$',
+        category: 'romance'
+    },
+    { 
+        name: 'The Sorrows of Young Werther', 
+        img: '/images/item16.jpg',
+        writer: 'Goethe', 
+        price: '40.00$',
+        category: 'classic' 
+    },
+    { 
+        name: 'Happy Place', 
+        img: '/images/item29.jpg',
+        writer: 'Emily Henry', 
+        price: '10.00$',
+        category: 'romance'
+    },
+    { 
         name: 'Harry Potter and the Prisoner of Azkaban', 
         img: '/images/item1.jpg',
         writer: 'J.K. Rowling', 
@@ -40,6 +83,13 @@ const books = [
         writer: 'Jose Saramago', 
         price: '40.00$',
         category: 'fiction'
+    },
+    { 
+        name: 'EAT, PRAY, LOVE', 
+        img: '/images/item14.jpg',
+        writer: 'Elizabeth Gilbert', 
+        price: '70.00$',
+        category: 'romance'
     },
     { 
         name: 'Kuyucaklı Yusuf', 
@@ -56,11 +106,60 @@ const books = [
         category: 'classic'
     },
     { 
+        name: 'Harry Potter and Deathly Hallows', 
+        img: '/images/item27.jpg',
+        writer: 'J.K. Rowling', 
+        price: '90.00$',
+        category: 'fiction'
+    },
+    { 
+        name: 'After You', 
+        img: '/images/item13.jpg',
+        writer: 'Jojo Moyes', 
+        price: '50.00$',
+        category: 'romance'
+    },
+    { 
         name: 'I, Robot', 
         img: '/images/item8.jpg',
         writer: 'Isaac Asimov', 
         price: '70.00$',
         category: 'fiction'
+    },
+    { 
+        name: 'Things We Never Got Over', 
+        img: '/images/item21.jpg',
+        writer: 'Lucy Score', 
+        price: '25.00$',
+        category: 'romance'
+    },
+    { 
+        name: 'Letters to Milena', 
+        img: '/images/item20.jpg',
+        writer: 'Franz Kafka', 
+        price: '30.00$',
+        category: 'classic'
+    },
+    { 
+        name: 'The Fault in Our Stars', 
+        img: '/images/item11.jpg',
+        writer: 'John Green', 
+        price: '40.00$',
+        category: 'romance'
+    },
+    { 
+        name: "The Hitchhiker's Guide to the Galaxy", 
+        img: '/images/item25.jpg',
+        writer: 'Douglas Adams', 
+        price: '50.00$',
+        category: 'fiction'
+    },
+    { 
+        name: 'Things We Hide From Light', 
+        img: '/images/item22.jpg',
+        writer: 'Lucy Score', 
+        price: '20.00$',
+        category: 'romance'
     },
     { 
         name: 'Masumiyet Müzesi', 
@@ -69,14 +168,41 @@ const books = [
         price: '80.00$',
         category: 'classic'
     },
+    { 
+        name: 'Harry Potter and the Order of The Phoenix', 
+        img: '/images/item23.jpg',
+        writer: 'J.K. Rowling', 
+        price: '90.00$',
+        category: 'fiction'
+    },
     {
-        name: '', 
-        img: '',
-        writer: '', 
-        price: '',
-        category: 'comics'
+        name: 'Me Before You', 
+        img: '/images/item12.jpg',
+        writer: 'Jojo Moyes', 
+        price: '55.00$',
+        category: 'romance'
+    },
+    {
+        name: 'The Picture of Dorian Gray', 
+        img: '/images/item18.jpg',
+        writer: 'Oscar Wilde', 
+        price: '25.00$',
+        category: 'classic'
+    },
+    { 
+        name: 'The Little Prince', 
+        img: '/images/item30.jpg',
+        writer: 'Antoine De Saint-Exupery', 
+        price: '10.00$',
+        category: 'classic'
+    },
+    { 
+        name: 'Harry Potter and the Goblet of Fire', 
+        img: '/images/item24.jpg',
+        writer: 'J.K. Rowling', 
+        price: '90.00$',
+        category: 'fiction'
     },
 ];
 
-// Data array of books
 export default books;
